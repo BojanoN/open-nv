455f9b466bdf08d3a2ee5d41cad3b4697bc973c5
Bojan Novkovic <bojan.novkovic@kset.org>
2020-05-11 22:44:23 +0200

    bsa header guards + test assertions

--- src/bsa/bsa.hpp
+++ src/bsa/bsa.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "util/enumflags.hpp"
 #include <cstdint>
 #include <fstream>
--- src/tests/bsatest.cpp
+++ src/tests/bsatest.cpp
@@ -1,6 +1,7 @@
 #include "bsa/bsa.hpp"
 #include "bsa/hash.hpp"
 #include "logc/log.h"
+#include <cassert>
 
 int main(void)
 {
@@ -21,20 +22,28 @@ int main(void)
     // Uncompressed file test
     // Should succeed
     std::vector<uint8_t>* ret = bsa->getFile("meshes\\architecture\\mccarran\\mcmarranwallsdes\\walllong.nif");
-    // Should fail
-    bsa->getFile("meshes\\architecture\\mccaan\\mcmarranwallsdes\\walllong.nif");
+    assert(ret != nullptr);
     delete ret;
+
+    // Should fail
+    ret = bsa->getFile("meshes\\architecture\\mccaan\\mcmarranwallsdes\\walllong.nif");
+    assert(ret == nullptr);
+    delete ret;
+
     // Should be served from cache
     ret = bsa->getFile("meshes\\architecture\\mccarran\\mcmarranwallsdes\\walllong.nif");
+    assert(ret != nullptr);
     delete ret;
 
     std::string correctPath = "meshes\\architecture\\mccarran\\mcmarranwallsdes\\walllong.nif";
     std::string wrongPath   = "meshes\\architecture\\mccaan\\mcmades\\wallg.nif";
 
     bool exists = bsa->fileExists(correctPath);
+    assert(exists);
     log_debug("File %s exists in %s: %s", correctPath.c_str(), updateBSA.c_str(), exists ? "true" : "false");
 
     exists = bsa->fileExists(wrongPath);
+    assert(!exists);
     log_debug("File %s exists in %s: %s", wrongPath.c_str(), updateBSA.c_str(), exists ? "true" : "false");
 
     delete bsa;
@@ -49,6 +58,7 @@ int main(void)
     }
 
     ret = bsa->getFile("textures\\clutter\\snowglobes\\snowglobenelisafb_d.dds");
+    assert(ret != nullptr);
     delete ret;
 
     delete bsa;
