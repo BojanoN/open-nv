6f6e69c20cbbc8fafc411c52d93376189b46f93e
Andrej Mijić <andrej.mijic@fer.hr>
2020-08-11 17:00:27 +0200

    Some fixes

--- src/nif/nifactory.cpp
+++ src/nif/nifactory.cpp
@@ -1,30 +1,5 @@
 #include "nifactory.hpp"
 #include <string>
-#include <unordered_map>
-#include "nifreader.hpp"
-#include "main/main.hpp"
-#include <memory>
+class NiObject;
 
-std::unordered_map<std::string, std::shared_ptr<NiObject* (*)(NifReader&)>> NiFactory::createCreatorMap() {
-        std::unordered_map<std::string, NiObject* (*)(NifReader&)> m;
-		m["NiObject"] = &NiObject::create;
-		m["NiAVObject"] = &NiAVObject::create;
-		m["NiDynamicEffect"] = &NiDynamicEffect::create;
-		m["NiProperty"] = &NiProperty::create;
-		m["NiObjectNET"] = &NiObjectNET::create;
-		m["NiCollisionObject"] = &NiCollisionObject::create;
-		m["NiNode"] = &NiNode::create;
-		m["NiStringExtraData"] = &NiStringExtraData::create;
-		m["NiTriStrips"] = &NiTriStrips::create;
-		//m["NiTriStripsData"] = &NiTriStripsData::create;
-		m["BSShaderPPLightingProperty"] = &BSShaderPPLightingProperty::create;
-		m["BSShaderTextureSet"] = &BSShaderTextureSet::create;
-		m["BSShaderLightingProperty"] = &BSShaderLightingProperty::create;
-		m["BSShaderProperty"] = &BSShaderProperty::create;
-		m["NiShadeProperty"] = &NiShadeProperty::create;
-		m["NiMaterialProperty"] = &NiMaterialProperty::create;
-		m["NiAlphaProperty"] = &NiAlphaProperty::create;
-        return m;
-    }
-
-const std::unordered_map<std::string, std::shared_ptr<NiObject* (*)(NifReader&)>> NiFactory::creatorMap = createCreatorMap();
+const std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::creatorMap = createCreatorMap();
--- src/nif/nifactory.hpp
+++ src/nif/nifactory.hpp
@@ -1,37 +1,27 @@
-<<<<<<< a3c8b8e7d299c1e76723a2121f70dd14fb2ab692
 #pragma once
 
 #include <unordered_map>
+
+//#include "nifdata.hpp"
+#include "main/main.hpp"
 
 
 class NifReader;
-class NiObject;
 
 struct NiFactory {
 private:
-    static std::unordered_map<std::string, std::shared_ptr<NiObject* (*)(NifReader&)>> createCreatorMap();
-public:
-    static const std::unordered_map<std::string, std::shared_ptr<NiObject* ()(NifReader&)>> creatorMap;
-};
-
-//const std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::creatorMap = createCreatorMap();
-=======
-
-#pragma once
-#include "main/main.hpp"
-
-#include <unordered_map>
-
-struct NiFactory {
-private:
-    static std::unordered_map<std::string, NiObject* (*)(FILE*)> createCreatorMap() {
-        std::unordered_map<std::string, NiObject* (*)(FILE*)> m;
+    static std::unordered_map<std::string, NiObject* (*)(NifReader&)> createCreatorMap() {
+        std::unordered_map<std::string, NiObject* (*)(NifReader&)> m;
 		m["NiObject"] = &NiObject::create;
+		m["NiAVObject"] = &NiAVObject::create;
+		m["NiDynamicEffect"] = &NiDynamicEffect::create;
+		m["NiProperty"] = &NiProperty::create;
+		m["NiObjectNET"] = &NiObjectNET::create;
+		m["NiCollisionObject"] = &NiCollisionObject::create;
         return m;
     }
 public:
-    static const std::unordered_map<std::string, NiObject* (*)(FILE*)> creatorMap;
+    static const std::unordered_map<std::string, NiObject* (*)(NifReader&)> creatorMap;
 };
 
-const std::unordered_map<std::string, NiObject* (*)(FILE*)> NiFunctionMaps::creatorMap = createCreatorMap();
->>>>>>> Begin nif reading code, refactor generator
+//const std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::creatorMap = createCreatorMap();
