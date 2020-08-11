3f1e5002c20befc7dd5be98e6e55d6b18a13e707
Andrej Mijić <andrej.mijic@fer.hr>
2020-08-11 19:51:12 +0200

    Further fixes

--- src/nif/nifactory.cpp
+++ src/nif/nifactory.cpp
@@ -1,5 +1,20 @@
 #include "nifactory.hpp"
 #include <string>
-class NiObject;
+#include <unordered_map>
+#include "nifreader.hpp"
+#include "main/main.hpp"
+
+std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::createCreatorMap() {
+        std::unordered_map<std::string, NiObject* (*)(NifReader&)> m;
+		m["NiObject"] = &NiObject::create;
+		m["NiAVObject"] = &NiAVObject::create;
+		m["NiDynamicEffect"] = &NiDynamicEffect::create;
+		m["NiProperty"] = &NiProperty::create;
+		m["NiObjectNET"] = &NiObjectNET::create;
+		m["NiCollisionObject"] = &NiCollisionObject::create;
+		m["NiNode"] = &NiNode::create;
+		m["NiStringExtraData"] = &NiStringExtraData::create;
+        return m;
+    }
 
 const std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::creatorMap = createCreatorMap();
--- src/nif/nifpointer.hpp
+++ src/nif/nifpointer.hpp
@@ -2,12 +2,13 @@
 
 
 #include "nifdata.hpp"
-
+#include "nifreader.hpp"
 
 template <typename T>
 class NifPointer {
 
 private:
+	
 	union {
 		uint64_t index;
 		T* pointer;
@@ -21,8 +22,10 @@ public:
 	T* get(); 
 	NifPointer(uint64_t index) : index{index} {}
 	NifPointer(uint32_t index) : index{static_cast<uint64_t>(index)} {}
-
+	NifPointer() {}
 	void resolve(NifData& data);
+
+	void load(NifReader& reader);
 };
 
 
@@ -45,4 +48,49 @@ uint64_t NifPointer<T>::getIndex() {
 template <typename T>
 void NifPointer<T>::resolve(NifData& data) {
 	this->pointer = reinterpret_cast<T*>(data.getBlock(this->index));
+}
+
+template <typename T>
+void NifPointer<T>::load(NifReader& reader) {
+	uint32_t index;
+	reader.read(&index, sizeof(uint32_t), 1);
+	this->index = static_cast<uint64_t>(index);
+}
+
+
+template <typename T>
+class NifPointerList {
+private:
+	uint32_t length = 0;
+	union {
+		uint64_t* indices;
+		T** pointers;
+	};
+
+public:
+	T* get(uint32_t index) {return pointers[index];}
+	NifPointerList() {}
+
+	void resolve(NifData& data);
+	void load(NifReader& reader, uint32_t length);
+
+	T* operator[](uint32_t idx) { return pointers[idx]; }
+};
+
+template <typename T>
+void NifPointerList<T>::load(NifReader& reader, uint32_t length) {
+	this->length = length;
+	uint32_t* indices = new uint32_t[length];
+	reader.read(indices, sizeof(uint32_t), length);
+	this->indices = new uint64_t[length];
+	for(unsigned int i = 0; i < length; i++) {
+		this->indices[i] = static_cast<uint64_t>(indices[i]);
+	}
+}
+
+template <typename T>
+void NifPointerList<T>::resolve(NifData& data) {
+	for(unsigned int i = 0; i < this->length; i++) {
+		this->pointers[i] = reinterpret_cast<T*>(data.getBlock(this->indices[i]));
+	}
 }
