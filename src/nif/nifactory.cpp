608dc3b3a2d08fc4bbd7091d5729b5ee5deba8ba
Andrej Mijić <andrej.mijic@fer.hr>
2020-08-11 19:51:12 +0200

    Further fixes

--- src/nif/nifactory.cpp
+++ src/nif/nifactory.cpp
@@ -3,9 +3,8 @@
 #include <unordered_map>
 #include "nifreader.hpp"
 #include "main/main.hpp"
-#include <memory>
 
-std::unordered_map<std::string, std::shared_ptr<NiObject* (*)(NifReader&)>> NiFactory::createCreatorMap() {
+std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::createCreatorMap() {
         std::unordered_map<std::string, NiObject* (*)(NifReader&)> m;
 		m["NiObject"] = &NiObject::create;
 		m["NiAVObject"] = &NiAVObject::create;
@@ -15,16 +14,7 @@ std::unordered_map<std::string, std::shared_ptr<NiObject* (*)(NifReader&)>> NiFa
 		m["NiCollisionObject"] = &NiCollisionObject::create;
 		m["NiNode"] = &NiNode::create;
 		m["NiStringExtraData"] = &NiStringExtraData::create;
-		m["NiTriStrips"] = &NiTriStrips::create;
-		//m["NiTriStripsData"] = &NiTriStripsData::create;
-		m["BSShaderPPLightingProperty"] = &BSShaderPPLightingProperty::create;
-		m["BSShaderTextureSet"] = &BSShaderTextureSet::create;
-		m["BSShaderLightingProperty"] = &BSShaderLightingProperty::create;
-		m["BSShaderProperty"] = &BSShaderProperty::create;
-		m["NiShadeProperty"] = &NiShadeProperty::create;
-		m["NiMaterialProperty"] = &NiMaterialProperty::create;
-		m["NiAlphaProperty"] = &NiAlphaProperty::create;
         return m;
     }
 
-const std::unordered_map<std::string, std::shared_ptr<NiObject* (*)(NifReader&)>> NiFactory::creatorMap = createCreatorMap();
+const std::unordered_map<std::string, NiObject* (*)(NifReader&)> NiFactory::creatorMap = createCreatorMap();
