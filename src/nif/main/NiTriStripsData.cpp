e4b3a8100c2e432dcd2f73230a39d745c8d19def
Andrej Mijić <andrej.mijic@fer.hr>
2020-08-14 00:04:30 +0200

    It's alive!

--- src/nif/main/NiTriStripsData.cpp
+++ src/nif/main/NiTriStripsData.cpp
@@ -5,8 +5,11 @@ NiTriStripsData::NiTriStripsData(NifReader& reader) : NiTriBasedGeomData(reader)
 	reader.read(stripLengths, sizeof(uint16_t), numStrips);
 	reader.read(&hasPoints, sizeof(nif_bool_t), 1);
 if(hasPoints && numStrips != 0) {
-	points = new uint16_t[numStrips];
-	reader.read(points, sizeof(uint16_t), numStrips);
+	points = new uint16_t*[numStrips];
+	for(unsigned int i = 0; i < numStrips; i++) {
+		points[i] = new uint16_t[stripLengths[i]];
+		reader.read(points[i], sizeof(uint16_t), stripLengths[i]);
+	}
 }
 }
 NiObject* NiTriStripsData::create(NifReader& reader) {
@@ -15,7 +18,10 @@ NiObject* NiTriStripsData::create(NifReader& reader) {
 NiTriStripsData::~NiTriStripsData() {
 	delete[]stripLengths;
 if(hasPoints && numStrips != 0) {
-	delete[]points;
+	for(unsigned int i = 0; i < numStrips; i++) {
+		delete[] points[i];
+	}
+	delete[] points;
 }
 }
 void NiTriStripsData::resolvePointers(NifData& data) {
