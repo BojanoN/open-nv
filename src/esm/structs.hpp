5eec6acf803de449a14a9e679d24790d09a2ddc8
Andrej Mijić <andrej.mijic@fer.hr>
2020-08-26 13:20:22 +0200

    Refactor model data for integration

--- src/esm/structs.hpp
+++ src/esm/structs.hpp
@@ -2,6 +2,7 @@
 
 #include "reader.hpp"
 #include "types.hpp"
+#include "subrecords/modeldata.hpp"
 
 #include <cstdint>
 #include <sstream>
@@ -54,39 +55,6 @@ struct FactRaceRelation {
     formid   faction;
     int32_t  modifier;
     uint32_t groupCombatReaction;
-};
-
-struct AlternateTexture {
-    uint32_t    nameLength;
-    std::string name;
-    formid      newTexture;
-    int32_t     index;
-};
-
-struct ModelData {
-    std::string                   filename;
-    uint8_t                       unused[4];
-    uint32_t                      alternateTextureCount;
-    std::vector<uint8_t>          textureHashes;
-    std::vector<AlternateTexture> alternateTextures;
-    uint8_t                       FaceGenModelFlags;
-
-    static void load(ESMReader& reader, ModelData& modelData, int index);
-
-private:
-    const static constexpr ESMType filenameType[]    = { ESMType::MODL, ESMType::MOD2, ESMType::MOD3, ESMType::MOD4 };
-    const static constexpr ESMType unusedType[]      = { ESMType::MODB, ESMType::NO_ENTRY, ESMType::NO_ENTRY, ESMType::NO_ENTRY };
-    const static constexpr ESMType hashesType[]      = { ESMType::MODT, ESMType::MO2T, ESMType::MO3T, ESMType::MO4T };
-    const static constexpr ESMType altTexturesType[] = { ESMType::MODS, ESMType::MO2S, ESMType::MO3S, ESMType::MO4S };
-    const static constexpr ESMType flagType[]        = { ESMType::MODD, ESMType::NO_ENTRY, ESMType::MOSD, ESMType::NO_ENTRY };
-
-    const static constexpr ESMType index_0[] = { ESMType::MODL, ESMType::MODB, ESMType::MODT, ESMType::MODS, ESMType::MODD };
-    const static constexpr ESMType index_1[] = { ESMType::MOD2, ESMType::MO2T, ESMType::MO2S };
-    const static constexpr ESMType index_2[] = { ESMType::MOD3, ESMType::MO3T, ESMType::MO3S, ESMType::MOSD };
-    const static constexpr ESMType index_3[] = { ESMType::MOD4, ESMType::MO4T, ESMType::MO4S };
-
-    const static constexpr ESMType* types[4] = { index_0, index_1, index_2, index_3 };
-    static bool                     isInCollection(uint32_t type, int index);
 };
 
 struct EffectData {
