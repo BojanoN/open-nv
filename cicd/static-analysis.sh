ce2ee31a32b7e9e29921627a4f5704ed9e74d939
Stjepan B <stjepanbrkicc@gmail.com>
2020-08-29 13:40:31 +0200

    finish work on static analysis script

--- cicd/static-analysis.sh
+++ cicd/static-analysis.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-ERROR=$(cppcheck ../src 3>&1 1>&2 2>&3 | tee /dev/stderr)  
-# ^ redirect stderr to ERROR variable while still printing both stdout and stderr
+ERROR="$(cppcheck ../src 3>&1 1>&2 2>&3 | tee /dev/stderr)"  
+# redirect stderr to ERROR variable while still printing both stdout and stderr
+echo "------------------------SUMMARY ------------------------"
 if [[ -z "${ERROR// }" ]]
 then
     echo "all OK"
 else
     # force CICD build to fail if there are error messages
-    echo "------------------------------"
-    echo "errors reported, build failing"
+    echo "errors reported, build failing:"
+    echo ""
+    echo -e "${ERROR}"
     exit 1
 fi
