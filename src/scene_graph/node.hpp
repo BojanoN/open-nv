b76167964cdb45d600f5cac4ca62824a5383afd3
Andrej Mijić <andrej.mijic@fer.hr>
2020-10-27 21:13:45 +0100

    Small scene graph proposal

--- src/scene_graph/node.hpp
+++ src/scene_graph/node.hpp
@@ -6,34 +6,238 @@
 
 #include <nif/main/NiGeometryData.hpp>
 
-#include <vector>
+#include <cstdint>
 
 namespace SceneGraph {
-class SceneNode {
+
+
+class TextureSet {
+    Texture2D* diffuse;
+    Texture2D* normalSpecular;
+    Texture2D* environmentMask;
+    Texture2D* glow;
+    Texture2D* parallax;
+    Texture2D* environment;
+};
+
+class NodeProperty {
+
+    virtual void apply();
+};
+
+class MaterialProperty : public NodeProperty {
+    Color3 specularColor;
+    Color3 emissiveColor;
+    float glossiness;
+    float alpha;
+    float emissiveMultiplier;
+};
+
+class LightingProperty : public NodeProperty {
+    uint32_t textureClampMode;
+    TextureSet& textures;
+    float parallaxMaxPasses;
+    float parallaxScale;
+    Color4 emissiveColor;
+};
+
+class AlphaProperty : public NodeProperty {
+    uint8_t threshold;
+}
+
+
+ 
+class Node {
+
+
 public:
-    SceneNode(NiGeometryData& g)
-        : geomData(g
+    
+    /*Node(NiGeometryData& g)
+        : geomData(g)
         , transform(1.)
         , scale(1.)
         , colour(1.)
-        , dirty(false) {};
-    ~SceneNode()
-    {
-        for (unsigned int i = 0; i < children.size(); i++) {
-            delete children[i];
+        , dirty(false) {}*/
+
+    Node(Matrix44& transformFromParent, uint32_t numProperties, NodeProperty* properties) : mNumProperties{numProperties} {
+        mProperties = new NodeProperty[numProperties];
+        for(unsigned int i = 0; i < numProperties; i++) {
+            mProperties[i] = properties;
+        }
+    }
+
+    virtual ~Node()
+    {   
+        if(mProperties) {
+            delete[] mProperties;
         }
     };
 
     virtual void update(float delta);
 
 protected:
-    NiGeometryData& geomData;
+    //NiGeometryData& geomData;
     Matrix44        transform;
-    Vector3         scale;
+    //Vector3         scale;
     Vector4         colour;
+
 
     bool dirty;
 
-    std::vector<SceneNode*> children;
+    uint32_t mNumProperties;
+    NodeProperty* mProperties;
+    //Node** children;
 };
+
+
+class GraphNode : public Node {
+
+public:
+
+    GraphNode(Matrix44& transformFromParent, uint32_t numChildren, Node** children) : Node(transformFromParent), mNumChildren{numChildren} {
+        mChildren = new Node*[numChildren];
+        for(unsigned int i = 0; i < numChildren; i++) {
+            mChildren[i] = children[i];
+        }
+    }
+
+    virtual ~GraphNode() {
+        if(mChildren) {
+            for (unsigned int i = 0; i < mNumChildren; i++) {
+                if(mChildren[i]) {
+                    delete mChildren[i];
+                }
+            }
+            delete[] children;
+        }
+    }
+
+protected:
+    uint32_t mNumChildren;
+    Node** mChildren;
 };
+
+struct GeometryData {
+
+  uint32_t mNumVertices;
+  Vector3* mVertices;
+  Vector3* mNormals;
+  Vector3* mTangents;
+  Vector3* mBitangents;
+  Color4* mColors;
+  TexCoord* mTexCoords;
+
+  GeometryData(uint32_t numVertices,
+           Vector3* vertices, Vector3* normals, Vector3* tangents,
+           Vector3* bitangents, TexCoord* texCoords, Color4* colors) :  mNumVertices{numVertices} {
+    mVertices = new Vector3[numVertices];
+    for (unsigned int i = 0; i < numVertices; i++) {
+      mVertices[i] = vertices[i];
+    }
+    if (normals) {
+      for (unsigned int i = 0; i < numVertices; i++) {
+        mNormals[i] = normals[i];
+      }
+    }
+    if (tangents) {
+      for (unsigned int i = 0; i < numVertices; i++) {
+        mTangents[i] = tangents[i];
+      }
+    }
+    if (bitangents) {
+      for (unsigned int i = 0; i < numVertices; i++) {
+        mBitangents[i] = bitangents[i];
+      }
+    }
+    if (texCoords) {
+      for (unsigned int i = 0; i < numVertices; i++) {
+        mTexCoords[i] = texCoords[i];
+      }
+    }
+    if (colors) {
+      for (unsigned int i = 0; i < numVertices; i++) {
+        mColors[i] = colors[i];
+      }
+    }
+  }
+
+  ~GeometryData() {
+    if (mVertices) {
+      delete[] mVertices;
+    }
+    if (mNormals) {
+      delete[] mNormals;
+    }
+    if (mTangents) {
+      delete[] mTangents;
+    }
+    if (mBitangents) {
+      delete[] mBitangents;
+    }
+    if (mTexCoords) {
+      delete[] mTexCoords;
+    }
+    if (mColors) {
+      delete[] mColors;
+    }
+  }
+
+};
+
+class LeafNode : public Node {
+ public:
+  LeafNode(Matrix44& transformFromParent, GeometryData* geometryData)
+      : Node(transformFromParent), mGeometryData{geometryData} {
+
+  }
+
+  virtual ~LeafNode() {
+    if(geometryData) {
+        delete geometryData;
+    }
+  }
+
+ protected:
+    GeometryData* geometryData;
+}
+
+class TriStripsNode : public LeafNode {
+
+public:
+
+    TriStripsNode(Matrix44& transformFromParent, GeometryData* geometryData, uint16_t numStrips, uint16_t* stripLengths, uint16_t** points) : LeafNode(transformFromParent, geometryData), mNumStrips{numStrips} {
+        mStripLengths = new uint16_t[numStrips];
+        mPoints = new uint16_t*[numStrips];
+        
+        for(unsigned int i = 0; i < numStrips; i++) {
+            
+            mStripLengths[i] = stripLengths[i];
+            mPoints[i] = new uint16_t[stripLengths[i]];
+            
+            for(unsigned int j = 0; j < stripLengths[i]; j++) {
+                mPoints[i][j] = points[i][j];    
+            }
+        }
+    }
+
+    virtual ~TriStripsNode() {
+        if(mStripLengths) {
+            delete[] mStripLengths;
+        }
+        if(mPoints) {
+            for(unsigned int i = 0; i < mNumStrips; i++) {
+                if(mPoints[i]) {
+                    delete[] mPoints[i];
+                }
+            }
+            delete[] mPoints;
+        }
+    }
+
+protected:
+    uint16_t mNumStrips;
+    uint16_t* mStripLengths;
+    uint16_t** mPoints;
+};
+
+};  // namespace SceneGraph
